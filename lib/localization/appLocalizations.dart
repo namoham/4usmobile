347cb54f2b445d22f1eabf521e3a5b6341f90588
Namarig Mohammed <57058210+namoham@users.noreply.github.com>
2023-06-03 01:36:55 -0400

    Add files via upload

---
+++ lib/localization/appLocalizations.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+class AppLocalizations {
+  Locale locale;
+
+  AppLocalizations(this.locale);
+
+
+  static AppLocalizations of( BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  static Map<String, String> _loadedLocalizedValues = {
+  };
+
+  Future loadLanguage() async {
+    String _langFile = await rootBundle.loadString('assets/lang/${locale.languageCode}.json');
+    Map<String, dynamic> _loadedValues = jsonDecode(_langFile);
+    _loadedLocalizedValues = _loadedValues.map((key, value) => MapEntry(key, value.toString()));
+  }
+
+  String getTranslated(String key) {
+    return _loadedLocalizedValues[key];
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate = AppLocalizationsDelegate();
+
+}
+
+class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+  const AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en', 'ar', 'fr'].contains(locale.languageCode);
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async{
+    AppLocalizations appLocalization = AppLocalizations(locale);
+    await appLocalization.loadLanguage();
+    return appLocalization;
+  }
+
+  @override
+  bool shouldReload(AppLocalizationsDelegate old) => false;
+}
