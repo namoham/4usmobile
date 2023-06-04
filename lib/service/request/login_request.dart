ac195c01f5a38c91a459757a731f925fd0d5f804
Namarig Mohammed <57058210+namoham@users.noreply.github.com>
2023-06-04 01:01:46 -0400

    Add files via upload

---
+++ lib/service/request/login_request.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+import 'package:mobile4us_new_version/dao/app_helpers/user_helper.dart';
+import 'package:mobile4us_new_version/models/user.dart';
+
+class LoginRequest {
+  UserHelper con = new UserHelper();
+
+  Future<User> getLogin(String username, String password) {
+    var result = con.getLogin(username,password);
+    return result;
+  }
+}
