0db63fb246e6eddfedcebf2bf8eff111f34ad401
Namarig Mohammed <57058210+namoham@users.noreply.github.com>
2023-06-04 01:08:53 -0400

    Add files via upload

---
+++ lib/service/response/login_response.dart
@@ -0,0 +1,21 @@
+
+import 'package:mobile4us_new_version/models/user.dart';
+import 'package:mobile4us_new_version/service/request/login_request.dart';
+
+abstract class LoginCallBack {
+  void onLoginSuccess(User user);
+  void onLoginError(String error);
+}
+
+class LoginResponse {
+  LoginCallBack _callBack;
+  LoginRequest loginRequest = new LoginRequest();
+  LoginResponse(this._callBack);
+
+  doLogin(String username, String password) {
+    loginRequest
+        .getLogin(username, password)
+        .then((user) => _callBack.onLoginSuccess(user))
+        .catchError((onError) => _callBack.onLoginError(onError.toString()));
+  }
+}
